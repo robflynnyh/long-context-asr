2a85751b0b1f92f79bfb10461e7e639bd222c104
Rob Flynn <rjflynn2@sheffield.ac.uk>
2023-06-01 10:49:07 +0100

    brr

---
+++ job_scripts/preprocess/stage_1/create_and_run.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+TOPDIRS = [f'{el}' for el in range(8)]
+BASEPATH = '/mnt/parscratch/users/acp21rjf/'
+
+def create_bash_file(path):
+    '''
+    create bash file to run on cluster
+    '''
+    save_name = path.replace('/', '_') + '.sh'
+    with open(save_name, 'w') as f:
+        f.write(\
+f'''#!/bin/bash
+#SBATCH --time=03:30:00
+#SBATCH --mem=16GB
+
+module load Anaconda3/2022.10
+source activate a100
+python -m lcasr.utils.preprocess --ogg_path {path} --stage 0
+        ''')
+    return save_name
+        
+for topdir in TOPDIRS:
+    path = os.path.join(BASEPATH, topdir)
+    folders = os.listdir(path)
+    for folder in folders:
+        path = os.path.join(BASEPATH, topdir, folder)
+        bfile = create_bash_file(path)
+        # lauch job
+        subprocess.run(['sbatch', bfile])
+
+
+
+
+
--- lcasr/utils/preprocess.py
+++ lcasr/utils/preprocess.py
@@ -1,38 +1,51 @@
-import audio_tools
+from lcasr.utils import audio_tools
 import argparse
 import os
 from os.path import join
 from tqdm import tqdm
 import torch
 
+def del_spectograms(ogg_path):
+    '''
+    delete all spectograms in all folders
+    '''
+    ogg_dir = ogg_path
+    for top_level in [f'{el}' for el in range(8)]:
+        print(f'processing {top_level}')
+        path = join(ogg_dir, top_level)
+        folders = os.listdir(path)
+        for folder in tqdm(folders, desc='folders'):
+            path = join(ogg_dir, top_level, folder)
+            shows = os.listdir(path)
+            for show in tqdm(shows, desc='shows'):
+                path = join(ogg_dir, top_level, folder, show)
+                episodes = os.listdir(path)
+                for ogg in episodes:
+                    path = join(ogg_dir, top_level, folder, show, ogg)
+                    if path.endswith('.spec.pt'):
+                        os.remove(path)
+
 def stage_1(args):
     '''
     In this stage we convert ogg files to spectograms and save them in the same folder with same name except with file extension .spec.pt
     '''
     ogg_dir = args.ogg_path
-    top_level_split = args.top_level_split
-
-    path = join(ogg_dir, top_level_split)
-    folders = os.listdir(path)
-    for folder in tqdm(folders, desc='folders'):
-        path = join(ogg_dir, top_level_split, folder)
-        shows = os.listdir(path)
-        for show in tqdm(shows, desc='shows'):
-            path = join(ogg_dir, top_level_split, folder, show)
-            episodes = os.listdir(path)
-            for ogg in episodes:
-                path = join(ogg_dir, top_level_split, folder, show, ogg)
-                if path.endswith('.ogg'):
-                    out_path = path.replace('.ogg', '.spec.pt')
-                    if not os.path.exists(out_path):
-                        spectogram = audio_tools.processing_chain(path)
-                        torch.save(spectogram, out_path)
+    shows = os.listdir(ogg_dir)
+    for show in tqdm(shows, desc='shows'):
+        path = join(ogg_dir, show)
+        episodes = os.listdir(path)
+        for ogg in episodes:
+            path = join(ogg_dir, show, ogg)
+            if path.endswith('.ogg'):
+                out_path = path.replace('.ogg', '.spec.pt')
+                if not os.path.exists(out_path):
+                    spectogram = audio_tools.processing_chain(path).to(torch.float16)
+                    torch.save(spectogram, out_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--ogg_path', type=str, default='')
-    parser.add_argument('--top_level_split', type=str, default='0', help='corpus is partitioned into folders 0-7, this dictates which folder to process, this is done to parallelize the process')
     parser.add_argument('--stage', type=str, default='0', help='0: convert ogg to spectograms')
 
     args = parser.parse_args()
