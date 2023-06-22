a5deef610cb21cb3aad0539068b9e0f8b7af5126
Rob Flynn <rjflynn2@sheffield.ac.uk>
2023-06-22 16:54:31 +0100

    remove job scripts

--- job_scripts/preprocess/stage_1/_mnt_parscratch_users_acp21rjf_spotify_audio_4_L.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=03:30:00
-#SBATCH --mem=32GB
-
-module load Anaconda3/2022.10
-source activate a100
-python -m lcasr.utils.preprocess --ogg_path /mnt/parscratch/users/acp21rjf/spotify/audio/4/L --stage 0
-        
