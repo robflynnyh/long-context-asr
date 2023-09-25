8788eed954c5b3e2cd6c0754a17700d002ed920e
Rob Flynn <rjflynn2@sheffield.ac.uk>
2023-09-25 16:15:55 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,24 @@ from setuptools import setup
 
 setup(
     name='lcasr',
-    version='0.0.0.1',    
+    version='0.0.9',    
     description='Code for training long-context asr models on spotify podcast corpus',
     url='https://github.com/robflynnyh/long-context-asr',
     author='Rob Flynn',
     author_email='rjflynn2@sheffield.ac.uk',
-    license='MIT',
+    license='Apache 2.0',
     packages=['lcasr'],
     install_requires=['torch',
                       'numpy',                     
                       ],
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',   
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
     ],
 )
