32e6f938c98d66e10b6e568b54d5957ae8442d4a
Rob Flynn <rjflynn2@sheffield.ac.uk>
2026-01-28 11:32:26 +0000

    latest

--- exp/run_launcher.py
+++ exp/run_launcher.py
@@ -22,8 +22,8 @@ source activate /mnt/parscratch/users/acp21rjf/conda/main/
 
 """,
     'h100':f"""#!/bin/bash\n
-#SBATCH --time=96:00:00
-#SBATCH --mem=130GB
+#SBATCH --time=60:00:00
+#SBATCH --mem=100GB
 #SBATCH --partition=gpu-h100
 #SBATCH --gres=gpu:1   
 #SBATCH --qos=gpu
